a53d79aa5713b63401306633b498cb41481d96a7
iskender-ctrl <82265032+iskender-ctrl@users.noreply.github.com>
2022-12-28 16:01:37 +0300

    Update

---
+++ Assets/Web3Unity/Scripts/Scenes/WebLogin.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+#if UNITY_WEBGL
+public class WebLogin : MonoBehaviour
+{
+    public Toggle rememberMe;
+
+    /*void Start() {
+        // if remember me is checked, set the account to the saved account
+        if(PlayerPrefs.HasKey("RememberMe") && PlayerPrefs.HasKey("Account"))
+        {
+            if (PlayerPrefs.GetInt("RememberMe") == 1 && PlayerPrefs.GetString("Account") != "")
+            {
+                // move to next scene
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }*/
+    [DllImport("__Internal")]
+    private static extern void Web3Connect();
+
+    [DllImport("__Internal")]
+    private static extern string ConnectAccount();
+
+    [DllImport("__Internal")]
+    private static extern void SetConnectAccount(string value);
+
+    private int expirationTime;
+    private string account;
+
+    public void OnLogin()
+    {
+        Login();
+        OnConnected();
+    }
+
+    async private void OnConnected()
+    {
+        account = ConnectAccount();
+        while (account == "")
+        {
+            await new WaitForSeconds(1f);
+            account = ConnectAccount();
+        };
+        // save account for next scene
+        PlayerPrefs.SetString("Account", account);
+        // reset login message
+        SetConnectAccount("");
+        // load next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    async public void Login()
+    {
+        // get current timestamp
+        int timestamp = (int)(System.DateTime.UtcNow.Subtract(new System.DateTime(1970, 1, 1))).TotalSeconds;
+        // set expiration time
+        int expirationTime = timestamp + 60;
+        // set message
+        string message = expirationTime.ToString();
+        // sign message
+        string signature = await Web3Wallet.Sign(message);
+        // verify account
+        string account = await EVM.Verify(message, signature);
+        int now = (int)(System.DateTime.UtcNow.Subtract(new System.DateTime(1970, 1, 1))).TotalSeconds;
+        // validate
+        if (account.Length == 42 && expirationTime >= now)
+        {
+            // save account
+            PlayerPrefs.SetString("Account", account);
+            if (rememberMe.isOn)
+                PlayerPrefs.SetInt("RememberMe", 1);
+            else
+                PlayerPrefs.SetInt("RememberMe", 0);
+            Debug.Log("Account: " + account);
+            // load next scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    public void OnSkip()
+    {
+        // burner account for skipped sign in screen
+        PlayerPrefs.SetString("Account", "");
+        // move to next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
+#endif
